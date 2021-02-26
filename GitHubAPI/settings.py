3cc6c56bdf6c575c0d30b43e24b73d2d798bc1d8
mudit mahajan <muditmhjn99@gmail.com>
2021-02-27 01:07:03 +0530

    security key fixed

--- GitHubAPI/settings.py
+++ GitHubAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9lw&nllbl&(jx3lgvvi5u=213r_j81h96rt+528^1)hi4^z4-h'
+SECRET_KEY = # key here
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
